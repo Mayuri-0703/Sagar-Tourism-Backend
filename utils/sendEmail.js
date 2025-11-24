3d024d3a8df821cfa356895dcaa6b473cb2b2ac1
Mayuri Pawar <mayurispawar03@gmail.com>
2025-11-24 22:06:05 +0530

    Fix gmail transporter

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,40 +1,10 @@
-// import nodemailer from "nodemailer";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// export const sendEmail = async ({ to, subject, html }) => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL,
-//       pass: process.env.APP_PASSWORD,
-//     },
-//   });
-
-//   try {
-//     const info = await transporter.sendMail({
-//       from: process.env.EMAIL,
-//       to,
-//       subject,
-//       html,
-//     });
-
-//     console.log("📩 Email sent to:", to, info.response);
-//     return info;
-//   } catch (error) {
-//     console.error("❌ Email error:", error);
-//     throw error; // important to catch errors in the controllers
-//   }
-// };
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",  
     auth: {
       user: process.env.EMAIL,
       pass: process.env.APP_PASSWORD,
